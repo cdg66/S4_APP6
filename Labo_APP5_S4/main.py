981434e3591ba4050135a3641417929eb4e45d64
Felix8665 <92393635+Felix8665@users.noreply.github.com>
2023-03-31 17:41:18 -0400

    graph fonctionnel

--- Labo_APP5_S4/main.py
+++ Labo_APP5_S4/main.py
@@ -9,7 +9,7 @@ from creation_Filtrage_FIR import *
 from Creation_deux_sinus import*
 from Filtrage_signaux import*
 #from format_QX.Y import *
-plt.ion()  # Comment out if using scientific mode!
+# plt.ion()  # Comment out if using scientific mode!
 fe = 20000
 #a,b,h_dft,w = fct_filtre_elliptique(fe)
 #z, p, k = fonction_zplane(a, b)
@@ -20,10 +20,10 @@ fe = 20000
 
 #fct_affiche_coeffcient(fonction_transfert_sos,a,b)
 filtre_p_b,filtre_p_h = filtre_FIR()
-Grand_X1,Grand_X2 = sinus()
-fct_filtrage_signaux(Grand_X1,Grand_X2,filtre_p_b,filtre_p_h)
-
-print('Done')
+# Grand_X1,Grand_X2 = sinus()
+# fct_filtrage_signaux(Grand_X1,Grand_X2,filtre_p_b,filtre_p_h)
+#
+# print('Done')
 
 
 
